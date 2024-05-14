6dbd478c5557101ba8691715e0d305e2f11c7e0a
Diana <dianaalokhina@gmail.com>
2024-05-14 16:33:41 -0400

    create navigation between pages and stay on SPA

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Admin() {
   return (
     <main>
       <h1>admin page</h1>
+      <p>
+        <Link href="/">Go to main page</Link>
+      </p>
     </main>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Main() {
   return (
     <main>
       <h1>main page</h1>
+      <p>
+        <Link href="/admin">Go to admin page </Link>
+      </p>
     </main>
   );
 }
