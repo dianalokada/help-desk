31aed4ae2ca945cb09df42bc7bfe9b71e1c98a49
Diana <dianaalokhina@gmail.com>
2024-05-14 13:18:18 -0400

    create admin and main page routes

---
+++ src/app/admin/page.js
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <main>
+      <h1>admin page</h1>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Main() {
   return (
-    <div>hello</div>
+    <main>
+      <h1>main page</h1>
+    </main>
   );
 }
